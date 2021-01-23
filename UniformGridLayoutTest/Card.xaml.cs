92cd7b53fa6fd66f4f20a54eaad7456b65fbaee5
Justin Liu <justin.xin.liu@gmail.com>
2021-01-22 22:27:03 -0800

    Add project files.

---
+++ UniformGridLayoutTest/Card.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UniformGridLayoutTest
+{
+    public sealed partial class Card : UserControl
+    {
+        public Card()
+        {
+            InitializeComponent();
+        }
+
+        public string CardName
+        {
+            get => (string)GetValue(AppNameProperty);
+            set => SetValue(AppNameProperty, value);
+        }
+        public static readonly DependencyProperty AppNameProperty = DependencyProperty.Register(
+            nameof(CardName), typeof(string), typeof(Card), new PropertyMetadata(default(string)));
+    }
+}
---
+++ UniformGridLayoutTest/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace UniformGridLayoutTest
+{
+    public class Entity
+    {
+        public Entity(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+
+    public sealed partial class MainPage : Page
+    {
+        private readonly List<Entity> _entities = new List<Entity> 
+        {
+            new Entity(1),
+            new Entity(2),
+            new Entity(3),
+            new Entity(4)
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
